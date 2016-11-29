8eb9338cf2284cdda4e4f1ca02e3ee3d925ff77f
Sly Flores <sly@christian.com.ph>
2016-11-29 22:17:39 +0800

    account is activated

--- incats.sql
+++ incats.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2016 at 04:50 PM
+-- Generation Time: Nov 29, 2016 at 10:15 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.4.31
 
@@ -42,25 +42,37 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `built_in` int(1) NOT NULL,
+  `is_activated` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `account_type`, `student_id`, `first_name`, `middle_name`, `last_name`, `gender`, `address`, `contact_no`, `birthdate`, `age`, `username`, `password`, `email`, `built_in`) VALUES
-(1, 'Administrator', '', 'Admin', '', '', '', '', '', '0000-00-00', 0, 'admin', 'admin', '', 1),
-(2, 'Applicant', '131-0428-5', 'Mary Claire', 'Senilla', 'Macabenta', 'Female', 'Balaoan', '09128818668', '1996-08-11', 20, 'claire', 'claire', 'clairemacabenta19@gmail.com', 2147483647),
-(3, 'Applicant', '131-0428-2', 'Keesha', 'Vega', 'Javier', 'Female', 'Bacnotan', '09498439978', '1996-11-27', 19, 'Keesha', 'javier', 'keeshajavier@yahoo.com', 2147483647),
-(4, 'Applicant', '131-0441-5', 'pamela', 'alcantara', 'ledda', 'Female', 'Bacnotan', '09461234671', '1995-09-17', 21, 'pamela', 'ledda', 'pamelaledda@yahoo.com', 2147483647),
-(5, 'Applicant', '131-0432-8', 'renella', 'ledda', 'acosta', 'Female', 'Bauang', '09084563634', '1994-04-09', 22, 'nella', 'acosta', 'renella@yahoo.com', 2147483647),
-(6, 'Applicant', '131-0441-7', 'Mikkah', 'Haha', 'Corpuz', 'Female', 'bauang', '09306583857', '1996-12-06', 19, 'mikkah', 'mikkah', 'mikkah@yahoo.com', 2147483647),
-(7, 'Applicant', '131-0428-1', 'Kaye', 'kin', 'Chu', 'Female', 'Bacnotan', '09498439978', '1995-11-27', 20, 'kaye', 'kaye', 'kayeA@yahoo.com', 2147483647),
-(8, 'Applicant', '131-1242-9', 'John', 'ken', 'Lui', 'Male', 'Luna', '09087765657', '1998-03-06', 18, 'john', 'john', 'john@yahoo.com', 2147483647),
-(9, 'Applicant', '131-0436-9', 'jovy', 'accosta', 'abuan', 'Female', 'san fernando', '09464573435', '1995-07-08', 21, 'jovy', 'jovy', 'jovyabuan@yahoo.com', 2147483647),
-(10, 'Applicant', '131-0976-6', 'eman', 'joe', 'bautista', 'Male', 'agoo', '0936134388', '1998-01-02', 18, 'eman', 'eman', 'eman@yahoo.com', 2147483647),
-(11, 'Applicant', '', 'La mej', 'pirena', 'maldita', '', '', '', '0000-00-00', 0, 'mejo', 'mejo', 'mejo@gmail.com', 0);
+INSERT INTO `accounts` (`id`, `account_type`, `student_id`, `first_name`, `middle_name`, `last_name`, `gender`, `address`, `contact_no`, `birthdate`, `age`, `username`, `password`, `email`, `built_in`, `is_activated`) VALUES
+(1, 'Administrator', '', 'Admin', '', '', '', '', '', '0000-00-00', 0, 'admin', 'admin', '', 1, 1),
+(2, 'Applicant', '131-0428-5', 'Mary Claire', 'Senilla', 'Macabenta', 'Female', 'Balaoan', '09128818668', '1996-08-11', 20, 'claire', 'claire', 'clairemacabenta19@gmail.com', 2147483647, 1),
+(3, 'Applicant', '131-0428-2', 'Keesha', 'Vega', 'Javier', 'Female', 'Bacnotan', '09498439978', '1996-11-27', 19, 'Keesha', 'javier', 'keeshajavier@yahoo.com', 2147483647, 1),
+(4, 'Applicant', '131-0441-5', 'pamela', 'alcantara', 'ledda', 'Female', 'Bacnotan', '09461234671', '1995-09-17', 21, 'pamela', 'ledda', 'pamelaledda@yahoo.com', 2147483647, 1),
+(5, 'Applicant', '131-0432-8', 'renella', 'ledda', 'acosta', 'Female', 'Bauang', '09084563634', '1994-04-09', 22, 'nella', 'acosta', 'renella@yahoo.com', 2147483647, 1),
+(6, 'Applicant', '131-0441-7', 'Mikkah', 'Haha', 'Corpuz', 'Female', 'bauang', '09306583857', '1996-12-06', 19, 'mikkah', 'mikkah', 'mikkah@yahoo.com', 2147483647, 1),
+(7, 'Applicant', '131-0428-1', 'Kaye', 'kin', 'Chu', 'Female', 'Bacnotan', '09498439978', '1995-11-27', 20, 'kaye', 'kaye', 'kayeA@yahoo.com', 2147483647, 1),
+(8, 'Applicant', '131-1242-9', 'John', 'ken', 'Lui', 'Male', 'Luna', '09087765657', '1998-03-06', 18, 'john', '123', 'john@yahoo.com', 2147483647, 1),
+(9, 'Applicant', '131-0436-9', 'jovy', 'accosta', 'abuan', 'Female', 'san fernando', '09464573435', '1995-07-08', 21, 'jovy', 'jovy', 'jovyabuan@yahoo.com', 2147483647, 1),
+(10, 'Applicant', '131-0976-6', 'eman', 'joe', 'bautista', 'Male', 'agoo', '0936134388', '1998-01-02', 18, 'eman', 'eman', 'eman@yahoo.com', 2147483647, 1),
+(11, 'Applicant', '', 'La mej', 'pirena', 'maldita', '', '', '', '0000-00-00', 0, 'mejo', 'mejo', 'mejo@gmail.com', 0, 1),
+(12, 'Applicant', '', 'maria', 'andres', 'sabado', '', '', '', '0000-00-00', 0, 'maria', 'maria', 'maria@gmail.com', 0, 1),
+(13, 'Applicant', '131-1246-2', 'Mary', 'uce', 'cary', 'Female', 'BAUANG', '09127121222', '1996-11-24', 19, 'marycary', 'mary', 'cary@gmail.com', 2147483647, 1),
+(14, 'Applicant', '2424849', 'Mary Renella Mikkah Keesha Pamela', 'S B D V A', 'Macabenta Acosta Corpuz Javier Ledda', 'Female', 'Jan Jan lng sa tabi', '0912345678', '1996-11-27', 19, 'bastauser', '1234', 'sbdva@ayoko.nga', 2147483647, 1),
+(15, 'Applicant', '131-1234-2', 'Joseph', 'Avatar', 'Patacsil', 'Male', 'kahit saan', '09090909090', '1996-11-23', 19, 'avatar', 'avatar', 'avatar@yahoo.com', 2147483647, 1),
+(16, 'Applicant', '', 'pam', 'ala', 'bee', '', '', '', '0000-00-00', 0, 'pam', 'pam', 'pam@gmailcom', 0, 1),
+(17, 'Applicant', '123', 'jake', 'c', 'alminiana', 'Male', 'sapilang', '9161212212', '1996-12-03', 19, 'jako', '123', 'dmmmsu@dmmmsu.edu.ph', 2147483647, 1),
+(18, 'Applicant', '', 'robert', 'go', 'see', '', '', '', '0000-00-00', 0, 'robert', 'robert', 'robert@gmail.com', 0, 1),
+(19, 'Applicant', '', 'robert', 'go', 'see', '', '', '', '0000-00-00', 0, 'robert', 'robert', 'robert@gmail.com', 0, 1),
+(20, 'Applicant', '123456789', 'enrique', 'geneta', 'abad', 'Male', 'payocpoc sur', '09095679089', '2010-07-20', 6, 'enrique', 'enrique', 'geneta@yahoo.com', 2147483647, 1),
+(21, 'Applicant', '00000', 'erma', 'erosa', 'panganiban', 'Female', 'poblacion sur, caba, la union', '09102911866', '1983-01-02', 33, 'erma', 'honeybear010911', 'ermaerosa@yahoo.com', 2147483647, 1),
+(22, 'Applicant', '12345678', 'jonathan', 'javier', 'estilong', 'Male', 'majiadsdak', '0975789549', '1984-01-17', 32, 'javier', 'javier', 'jje@yahoo.com', 2147483647, 1);
 
 -- --------------------------------------------------------
 
@@ -81,8 +93,7 @@ CREATE TABLE IF NOT EXISTS `announcements` (
 --
 
 INSERT INTO `announcements` (`id`, `heading`, `content`, `announcement_date`) VALUES
-(1, 'radf', 'werqwfadfd', '2016-11-17'),
-(2, 'announcement', 'tests', '2016-11-17');
+(2, 'announcement', 'test', '2016-11-17');
 
 -- --------------------------------------------------------
 
@@ -96,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `dismiss_notifications` (
   `scholarship_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `scholarship_id` (`scholarship_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
 
 --
 -- Dumping data for table `dismiss_notifications`
@@ -128,7 +139,29 @@ INSERT INTO `dismiss_notifications` (`id`, `account_id`, `scholarship_id`) VALUE
 (23, 1, 4),
 (24, 1, 6),
 (25, 1, 8),
-(26, 1, 9);
+(26, 1, 9),
+(27, 13, 1),
+(28, 13, 5),
+(29, 13, 7),
+(31, 1, 13),
+(32, 1, 12),
+(33, 1, 12),
+(34, 1, 12),
+(35, 1, 12),
+(36, 16, 1),
+(37, 16, 5),
+(38, 16, 7),
+(40, 16, 13),
+(41, 16, 14),
+(42, 17, 5),
+(43, 17, 14),
+(45, 17, 7),
+(46, 17, 1),
+(47, 17, 13),
+(48, 1, 16),
+(49, 1, 18),
+(51, 1, 20),
+(52, 1, 21);
 
 -- --------------------------------------------------------
 
@@ -149,8 +182,7 @@ CREATE TABLE IF NOT EXISTS `events` (
 --
 
 INSERT INTO `events` (`id`, `heading`, `content`, `event_date`) VALUES
-(1, 'heading', 'adfdhadferg', '2016-11-17'),
-(2, 'test event', 'blablabla', '2016-11-17');
+(1, 'heading', 'adfdhadferg', '2016-11-17');
 
 -- --------------------------------------------------------
 
@@ -166,7 +198,7 @@ CREATE TABLE IF NOT EXISTS `requirements` (
   `doc_title` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `scholarship_id` (`scholarship_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `requirements`
@@ -183,7 +215,14 @@ INSERT INTO `requirements` (`id`, `scholarship_id`, `description`, `doc_rating`,
 (8, 8, 'COG', '87', 'download.PNG'),
 (9, 9, 'COG', '96', 'download.PNG'),
 (10, 10, 'COG', '96', 'download.PNG'),
-(11, 11, '90', '', 'download.PNG');
+(12, 12, 'COG', '90', 'download.PNG'),
+(13, 13, 'Multiple daw po yan', '90', 'download.PNG'),
+(14, 14, 'COG', '90', 'download.PNG'),
+(15, 15, 'tet', 'test', 'app.docx'),
+(16, 16, 'cog', '98', 'IMG_3301.JPG'),
+(17, 17, 'cog', '1.0', 'IMG_3299.JPG'),
+(18, 18, 'cog', '2.50', 'IMG_3465.JPG'),
+(20, 20, 'COG', '90', 'IMG_3301.JPG');
 
 -- --------------------------------------------------------
 
@@ -208,7 +247,7 @@ CREATE TABLE IF NOT EXISTS `scholarships` (
   `evaluation_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `account_id` (`account_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `scholarships`
@@ -216,7 +255,7 @@ CREATE TABLE IF NOT EXISTS `scholarships` (
 
 INSERT INTO `scholarships` (`id`, `account_id`, `application_type`, `programs`, `program`, `course`, `college`, `year_level`, `semester`, `school_year`, `status`, `status_date`, `evaluated`, `evaluation_date`) VALUES
 (1, 2, 'New', 'University', 'Academic', 'BSIS', 'College of Computer Studies', '2', '1', '2016-2017', 'Approved', '2016-11-14 09:02:51', 2147483647, '2016-11-14 09:02:51'),
-(2, 3, 'New', 'University', 'Academic', 'BS agriculture', 'College of Agriculture', '3', '2', '2016-2017', 'Pending', '2016-11-14 09:14:19', 2147483647, '2016-11-14 09:14:19'),
+(2, 3, 'New', 'University', 'Academic', 'BS agriculture', 'College of Agriculture', '3', '2', '2016-2017', 'Pending', '2016-11-14 09:14:19', 0, '2016-11-14 09:14:19'),
 (3, 4, 'New', 'Government', 'Local Code', 'BS agricultural engineering', 'College of engineering', '3', '1', '2017-2018', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (4, 5, 'New', 'Government', 'DA ACEF', 'BEED', 'College of Education', '4', '1', '2017-2018', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (5, 6, 'New', 'University', 'Dependent', 'BSIS', 'College of Information System', '2', '1', '2016-2017', 'Approved', '2016-11-14 09:22:57', 2147483647, '2016-11-14 09:22:57'),
@@ -225,7 +264,15 @@ INSERT INTO `scholarships` (`id`, `account_id`, `application_type`, `programs`, 
 (8, 9, 'New', 'Government', 'DA ACEF', 'BEED', 'College of Education', '3', '2', '2016-2017', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (9, 10, 'New', 'University', 'Dependent', 'BS Agroforestry', 'Institute of Agroforestry & Watershed Management', '2', '1', '2016-2017', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (10, 10, 'New', 'University', 'Dependent', 'BS Agroforestry', 'Institute of Agroforestry & Watershed Management', '2', '1', '2016-2017', 'Disapproved', '2016-11-14 09:47:21', 2147483647, '2016-11-14 09:47:21'),
-(11, 2, 'Renewal', 'University', 'Academic', 'BSIS', 'College of Computer Studies', '2', '1', '2016-2017', 'Approved', '2016-11-14 09:53:16', 2147483647, '2016-11-14 09:53:16');
+(12, 13, 'New', 'University', 'Academic', 'BSIS', 'College of Computer Studies', '2', '1', '2018-2019', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(13, 14, 'New', 'Government', 'DSWD', 'Of Course', 'College of Course', '5', '1', '2019-2020', 'Approved', '2016-11-22 13:28:14', 2147483647, '2016-11-22 13:28:14'),
+(14, 15, 'New', 'University', 'Dependent', 'BSA', 'college of agriculture', '2', '1', '2016-2017', 'Approved', '2016-11-22 17:27:47', 2147483647, '2016-11-22 17:27:47'),
+(15, 17, 'Renewal', 'University', 'Academic', 'bsit', 'cit', '5', '1', '2016-2017', 'Approved', '2016-11-23 10:08:04', 2147483647, '2016-11-23 10:07:48'),
+(16, 20, 'New', 'Government', 'Local Code', 'ComSci', 'bsit', '2', '1', '2019-2020', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(17, 21, 'New', 'Government', 'DSWD', 'BS Psychology', 'CaS', '1', '1', '2016-2017', 'Approved', '2016-11-23 16:03:16', 2147483647, '2016-11-23 16:03:16'),
+(18, 22, 'New', 'Government', 'CHED', 'bsit', 'cit', '1', '1', '2019-2020', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(20, 6, 'Renewal', 'University', 'Dependent', 'BSIS', 'College of Computer Studies', '2', '1', '2016-2017', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(21, 2, 'Renewal', 'University', 'Dependent', 'BSIT', 'CIT', '1', '1', '2016-2017', 'On-Process', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -241,7 +288,7 @@ CREATE TABLE IF NOT EXISTS `testing_results` (
   `testing_date` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `scholar_id` (`scholar_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `testing_results`
@@ -251,7 +298,10 @@ INSERT INTO `testing_results` (`id`, `scholar_id`, `testing_type`, `rating`, `te
 (1, 2, 'PT', '21', '2016-11-17'),
 (2, 4, 'CAT', '85', '2016-11-17'),
 (3, 5, 'PT', '90', '2016-11-17'),
-(4, 6, 'PT', '88', '2016-11-17');
+(4, 6, 'PT', '88', '2016-11-17'),
+(5, 3, 'CAT', '93', '2016-11-22'),
+(6, 14, 'PT', '101', '2016-11-22'),
+(7, 14, 'CAT', '2001', '2016-11-22');
 
 --
 -- Constraints for dumped tables
